fcb54ca317fde83acc3212505b53cd9a58cc88f4
vankit7 <vankit7@gmail.com>
2022-02-12 17:50:38 +0530

    Initial commit

---
+++ appointmentservice/src/test/java/com/bmc/appointmentservice/AppointmentserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bmc.appointmentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmentserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ bmc.doctorservice/src/main/java/com/bmc/doctorservice/DoctorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bmc.doctorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorServiceApplication.class, args);
+	}
+
+}
